1a68ce60f81cac7e7b57dfec3e7859eeeb9b7428
elenoid <elenoiddesign@gmail.com>
2018-02-06 22:52:04 +0100

    smooth scrolling

--- js/plugins.js
+++ js/plugins.js
@@ -23,100 +23,106 @@
 
 // Place any jQuery/helper plugins in here.
 
+// Loading Page Pop up.
 $( document ).ready(function() {
   $('#fig-willkommen').addClass( "js-hover-figcaption" );
 });
 
-$('.navbar-center').on('mouseenter', function() {
-  $('#fig-willkommen').removeClass( "js-hover-figcaption" );
-});
-
-$('.figure-width').on('mouseenter', function() {
-  $('#fig-willkommen').removeClass( "js-hover-figcaption" );
-});
-
-$('.navbar-center').on('mouseenter', function() {
-  $('#fig-willkommen').removeClass( "js-hover-figcaption" );
-});
-
-$('#mobile-menu').on('mouseenter', function() {
+// Mobile navbar
+$('#mobile-menu').on('click', function() {
   $('#navbar').addClass( "js-hover-navbar" );
 });
 
-$('#close').on('mouseenter', function() {
+$('#close').on('click', function() {
   $('#navbar').removeClass( "js-hover-navbar" );
 });
 
-/*
-$('#link-willkommen').on('mouseenter', function() {
-  $('#fig-willkommen').addClass( "js-hover-figcaption" );
+// Test
+$('#link-willkommen').on('click', function() {
+  $('.js-figcaption').removeClass( "js-hover-figcaption" );
 });
 
-$('#link-willkommen').on('mouseleave', function() {
-  $('#fig-willkommen').removeClass( "js-hover-figcaption" );
+// If cursor hovers over a figure all previous opened figures close.
+$('.figure-width').on('mouseenter', function() {
+  $('.js-figcaption').removeClass( "js-hover-figcaption" );
 });
-*/
 
-$('#link-neubau').on('mouseenter', function() {
+// Specific figure is opend
+$('#link-neubau').on('click', function() {
+  $('.js-figcaption').removeClass( "js-hover-figcaption" );
   $('#fig-neubau').addClass( "js-hover-figcaption" );
 });
 
-$('#link-neubau').on('mouseleave', function() {
-  $('#fig-neubau').removeClass( "js-hover-figcaption" );
-});
-
-$('#link-umbau').on('mouseenter', function() {
+$('#link-umbau').on('click', function() {
+  $('.js-figcaption').removeClass( "js-hover-figcaption" );
   $('#fig-umbau').addClass( "js-hover-figcaption" );
 });
 
-$('#link-umbau').on('mouseleave', function() {
-  $('#fig-umbau').removeClass( "js-hover-figcaption" );
-});
-
-$('#link-service').on('mouseenter', function() {
+$('#link-service').on('click', function() {
+  $('.js-figcaption').removeClass( "js-hover-figcaption" );
   $('#fig-service').addClass( "js-hover-figcaption" );
 });
 
-$('#link-service').on('mouseleave', function() {
-  $('#fig-service').removeClass( "js-hover-figcaption" );
-});
-
-$('#link-wasserenthärtung').on('mouseenter', function() {
+$('#link-wasserenthärtung').on('click', function() {
+  $('.js-figcaption').removeClass( "js-hover-figcaption" );
   $('#fig-wasserenthärtung').addClass( "js-hover-figcaption" );
 });
 
-$('#link-wasserenthärtung').on('mouseleave', function() {
-  $('#fig-wasserenthärtung').removeClass( "js-hover-figcaption" );
-});
-
-$('#link-blitzschutz').on('mouseenter', function() {
+$('#link-blitzschutz').on('click', function() {
+  $('.js-figcaption').removeClass( "js-hover-figcaption" );
   $('#fig-blitzschutz').addClass( "js-hover-figcaption" );
 });
 
-$('#link-blitzschutz').on('mouseleave', function() {
-  $('#fig-blitzschutz').removeClass( "js-hover-figcaption" );
-});
-
-$('#link-ausbildung').on('mouseenter', function() {
+$('#link-ausbildung').on('click', function() {
+  $('.js-figcaption').removeClass( "js-hover-figcaption" );
   $('#fig-ausbildung').addClass( "js-hover-figcaption" );
 });
 
-$('#link-ausbildung').on('mouseleave', function() {
-  $('#fig-ausbildung').removeClass( "js-hover-figcaption" );
-});
-
-$('#link-galerie').on('mouseenter', function() {
+$('#link-galerie').on('click', function() {
+  $('.js-figcaption').removeClass( "js-hover-figcaption" );
   $('#fig-galerie').addClass( "js-hover-figcaption" );
 });
 
-$('#link-galerie').on('mouseleave', function() {
-  $('#fig-galerie').removeClass( "js-hover-figcaption" );
-});
-
-$('#link-kontakt').on('mouseenter', function() {
+$('#link-kontakt').on('click', function() {
+  $('.js-figcaption').removeClass( "js-hover-figcaption" );
   $('#fig-kontakt').addClass( "js-hover-figcaption" );
 });
 
-$('#link-kontakt').on('mouseleave', function() {
-  $('#fig-kontakt').removeClass( "js-hover-figcaption" );
-});
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+    $('#navbar').removeClass( "js-hover-navbar" );  
+  });
